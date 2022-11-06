0bbd386e7d24cabeea30c8f45c2a58618fb16e80
CroxxN <saradgaihre19@gmail.com>
2022-11-06 22:10:16 +0545

    absolute of number with bits

--- absolute-conversion-bit/src/main.rs
+++ absolute-conversion-bit/src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
-    let number = -50i32;
+    let base = -50_i32;
+    let positive_base = 100_i32;
+    let number = !(base as u32) + 1;
+    let postive = (!(positive_base as u32) + 1) as i32;
+    println!("{number} positive conversion: {postive}");
 
 }
