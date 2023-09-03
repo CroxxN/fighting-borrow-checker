aee5e7a05b4a7b65831c967ddb796c563f0cb784
CroxxN <saradgaihre19@gmail.com>
2023-09-03 20:23:00 +0545

    cast to const to mut; unsafe

--- memory/src/main.rs
+++ memory/src/main.rs
@@ -1,5 +1,16 @@
+struct Memory {
+    storge: i32,
+}
+
 fn main() {
+    const _THISER: Memory = Memory { storge: 32 };
     let number = &42 as *const i32 as *mut i32;
+    let numberical = &34;
+    let _num = numberical as *const i32 as *mut i32;
     let deferenced = unsafe { *number + 2 };
     println!("{deferenced} also {}", unsafe { *number });
+}
+
+fn _make() {
+    let _this = &mut "cout";
 }
